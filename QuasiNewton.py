83d26450de5a4cfd69ff075b566755476a3c2f20
Visamb <57657892+Visamb@users.noreply.github.com>
2020-09-15 23:42:29 +0200

    Add files via upload

--- QuasiNewton.py
+++ QuasiNewton.py
@@ -6,17 +6,37 @@ Takes a problem as input, where problem is  an instance of Optimizationproblem. 
 of solutionmethod. ONLY things that have to do with the methods.
 """
     def __init__(self,problem):
-        self.problem = problem
-        self.gradient = """DEFINE GRADIENT"""
-        self.Hessian = """Define HESSIAN"""
-        self.alpha = 1
+            self.alpha = 0.0001  # step size
+            self.f = problem.function# object function
+            self.gradient = problem.gradient#object gradient function
+            self.n = 2  # the dimension of the domain, R^n
+
+        def gradient(self, x):
+            """
+            Computes the gradient of f at the given point x by forward-difference (p.195 Nocedal, Wright)
+            :return: (array)
+            """
+            g = empty((self.n, 1))
+            for i in range(self.n):  # for every coordinate of x
+                e = zeros((self.n, 1))  # unit vectors in the domain
+                e[i] = self.alpha  # we want to take a step in the i:th direction
+                g[i] = (self.f(x + self.alpha * e) - self.f(x)) / self.alpha  # (8.1) at p.195
+
+            return g
+
+        def hessian(self):
+            """Computes hessian of f at the given point x """
+            return H
+
+        
+
     def newstep(self,Hessian,x, alpha):
         """Takes a step"""
         return x-alpha*Hessian*self.gradient
 
     def newHessian():
         """Returns Hessian. Overridden in 9 special methods"""
-        return self.Hessian
+        return
 
     def exactlinesearch():
         """Defines exact line search"""
