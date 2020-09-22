37ac713247588ea9ee88a738dd8f4021b8366a76
Visamb <57657892+Visamb@users.noreply.github.com>
2020-09-22 16:40:28 +0200

    Add files via upload

--- OptimizationProblem.py
+++ OptimizationProblem.py
@@ -1,12 +1,16 @@
 # Its constructor should take an
 # objective function as input, optionally also its gradient can be provided.
 
+from numpy import array
+
 
 class OptimizationProblem:
     """
     Defines a problem, independent of solution method.
     """
 
-    def __init__(self, function, gradient=[]):
+    def __init__(self, function, gradient=None, dimension=2, lsm="inexact"):
         self.function = function
         self.gradient = gradient
+        self.dimension = dimension
+        self.lsm = lsm
--- QuasiNewton.py
+++ QuasiNewton.py
@@ -1,129 +1,99 @@
-"""Generic class for QuasiNewton"""
-from numpy import *
-from math import inf
-from linesearchmethods import exact_linesearch, inexact_linesearch
+from Newton import *
 
 
-class QuasiNewton:
+class QuasiNewton(Newton):
+    """
+    Subclass to Newton, collecting the common methods for the deriving QuasiNewton methods (below this class)
+    """
 
-    def __init__(self, problem, lsm="inexact"):
-        self.epsilon = 0.001      # step size
-        self.f = problem.function  # object function
-        self.n = 2                 # the dimension of the domain, R^n
-        self.alpha = 5             #
-        self.values = array([])    #
-        self.TOL = 1.e-8           # tolerance for the 0-element
-
-        # Determining LSM
-        valid_lsm = ["inexact", "exact"]  # valid line search methods
-        if lsm not in valid_lsm:
-            raise ValueError("Provided LSM \'{}\' does not exist, choose between \'inexact\' or \'exact\'".format(lsm))
-        elif lsm == "inexact":
-            self.lsm = lsm
-
-            # PARAMETERS FOR INEXACT LINE SEARCH METHOD (slide 3.12 in Lecture03)
-            self.rho = 0.1
-            self.sigma = 0.7
-            self.tau = 0.1
-            self.chi = 9
-
-        elif lsm == "exact":
-            self.lsm = lsm
-
-    def gradient(self, x):
+    def update_hessian(self, x_next, x_k):
         """
-        Computes the gradient of f at the given point x by central-difference ((8.7) p.196 Nocedal, Wright)
-        :return: (array)
+        Abstract method for updating self.hessian
+        :param x_next: (array) to
+        :param x_k: (array) from
+        :return: None
         """
-        g = empty((self.n, 1))
-        for i in range(self.n):  # for every coordinate of x
-            e = zeros((self.n, 1))  # unit vectors in the domain
-            e[i] = self.epsilon  # we want to take a step in the i:th direction
-            g[i] = (self.f(x + e) - self.f(x - e)) / (2 * self.epsilon)  # (8.1) at p.195
-        return g
-
-    def hessian(self, x):
-        """
-        Computes the inverse hessian of f at the given point x by forward-difference (p.201 Nocedal, Wright)
-        :return: nxn matrix
-        """
-        G = empty((self.n, self.n))
-        for i in range(self.n):
-            for j in range(self.n):
-                direction1 = zeros((self.n, 1))
-                direction2 = zeros((self.n, 1))
-                direction1[i] = self.epsilon
-                direction2[j] = self.epsilon
-                G[i, j] = (self.f(x + direction1 + direction2) - self.f(x + direction2) - self.f(
-                    x + direction1) + self.f(x)) / (self.epsilon ** 2)
-        G = (G + G.transpose()) / 2
-        return G
-
-    def newton_direction(self, x):
-        """
-
-        :param x:
-        :return:
-        """
-
-        inverse_hessian = linalg.inv(self.hessian(x))
-        g = self.gradient(x)
-        newton_direction = -inverse_hessian.dot(g)
-        return newton_direction
 
     def newstep(self, x):
-        """
-        Computes coordinates for the next step in accordance with the Quasi Newton procedure.
-        :return: (array)
-        """
-
-        newton_direction = self.newton_direction(x)  # The Newton direction determines step direction.
-        new_coordinates = x + newton_direction*self.alpha
-
-
+        s = self.step_direction(x)
+        new_coordinates = x + self.alpha * s
+        self.update_hessian(new_coordinates, x)
         return new_coordinates
 
-    def linesearch(self, x):
-        if self.lsm == 'inexact':
-            return inexact_linesearch(self.f, x, self.newton_direction(x), self.rho, self.sigma, self.tau, self.chi)
-        elif self.lsm == 'exact':
-            return exact_linesearch(self.f, x, self.newton_direction(x))
 
-    def newHessian(self):
-        """Returns Hessian. Overridden in 9 special methods"""
-        return
+class GoodBroyden(QuasiNewton):  # 3.17
+    """
+    Uses simple Broyden rank-1 update of the Hessian G and applies Sherman-Morisson's formula,
+    see sections 3.15-3.17
+    """
 
-    def termination_criterion(self, x):
-        """
-        Asserts that the criterions for optimum are fulfilled. The criterions are:
-        1.Hessian is symmetric and positive definite.
-        2. The gradient is zero.
-        :return: boolean that is true if criteria are fulfilled
-        """
-        Hess = self.hessian(x)
-        if not all(self.gradient(x) < self.TOL):
-            return False
+    def update_hessian(self, x_next, x_k):
+        delta_k = x_next - x_k
+        gamma_k = self.gradient(x_next) - self.gradient(x_k)
+        if (gamma_k == zeros((2, 1))).all():
+            #self.inverted_hessian =
+            return
 
-        else:
-            try:
-                linalg.cholesky(Hess)
-                print("POSITIVE DEFINITE AND SYMMETRIC")
-            except:
-                return False  # If the Choleskymethod gives error False is returned.
+        H_km1 = self.inverted_hessian  # "H k minus 1"
+        u = H_km1@delta_k
+        nominator = (delta_k - H_km1@delta_k) @ u.T
+        denominator = u.T @ gamma_k
+        #H_k = H_km1 + outer(delta_k - H_km1@gamma_k, H_km1@gamma_k) / inner((H_km1@delta_k), gamma_k)
+        H_k = H_km1 + nominator/denominator
 
-        print("SOLVED!")
-        return True
+        self.inverted_hessian = H_k
 
-    def solve(self):
-        x = ones((self.n, 1)) * 4
-        solved = self.termination_criterion(x)
-        value = x
-        self.alpha = self.linesearch(value)
-        print(self.alpha)
-        self.values = value
-        while solved is False:
-            newvalue = self.newstep(value)
-            value = newvalue
-            solved = self.termination_criterion(value)
-            self.values = hstack([self.values, value])
-        return [value, self.f(value)]
+
+class BadBroyden(QuasiNewton):  # 3.18
+    def update_hessian(self, x_next, x_k):
+        delta_k = x_next - x_k
+        gamma_k = self.gradient(x_next) - self.gradient(x_k)
+        H_km1 = self.inverted_hessian  # "H k minus 1"
+        nominator = delta_k - H_km1@gamma_k
+        denominator = gamma_k.T@gamma_k
+        H_k = H_km1 + outer(nominator/denominator, gamma_k)
+
+        self.inverted_hessian = H_k
+
+
+class SymmetricBroyden(QuasiNewton):  # 3.20
+    def update_hessian(self, x_next, x_k):
+        delta_k = x_next - x_k
+        gamma_k = self.gradient(x_next) - self.gradient(x_k)
+        H_km1 = self.inverted_hessian  # "H k minus 1"
+        u = delta_k - H_km1@gamma_k
+        a = 1 / inner(u, gamma_k)
+        H_k = H_km1 + a*outer(u, u)
+
+        self.inverted_hessian = H_k
+
+
+class DFP(QuasiNewton):  # 3.21
+    def update_hessian(self, x_next, x_k):
+        delta_k = x_next - x_k
+        gamma_k = self.gradient(x_next) - self.gradient(x_k)
+        H_km1 = self.inverted_hessian  # "H k minus 1"
+        a = outer(delta_k,delta_k)
+        b = transpose(delta_k)@gamma_k
+        c = H_km1 @ gamma_k @ transpose(gamma_k) @ H_km1
+        d = transpose(gamma_k)@H_km1@gamma_k
+        H_k = H_km1 + a/b - c/d
+
+        self.inverted_hessian = H_k
+
+
+class BFGS(QuasiNewton):  # 3.22
+    def update_hessian(self, x_next, x_k):
+        delta_k = x_next - x_k
+        gamma_k = self.gradient(x_next) - self.gradient(x_k)
+        H_km1 = self.inverted_hessian  # "H k minus 1"
+        a = transpose(gamma_k)@H_km1@gamma_k
+        b = transpose(delta_k)@gamma_k
+        c = delta_k@transpose(delta_k)
+        d = transpose(delta_k)@gamma_k
+        f = delta_k@transpose(gamma_k)@H_km1+H_km1@gamma_k@transpose(delta_k)
+        g = transpose(delta_k)@gamma_k
+        H_k = H_km1 + (1 + a/b)*(c/d)-(f/g)
+
+        self.inverted_hessian = H_k
+
